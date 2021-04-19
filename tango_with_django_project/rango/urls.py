73d529ce26c2e4e755e76b5cc6cb5f19e1c04fb0
Margonite1 <80271893+Margonite1@users.noreply.github.com>
2021-04-19 10:32:10 +0100

    part 4

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -4,4 +4,6 @@ app_name = 'rango'
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('static/', views.static, name='static'),
+    path('static/images/rango.jpg', views.rangojpg, name='static/image/rango.jpg')
 ]
--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -18,14 +18,21 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('rango/', include('rango.urls')),
     path('about/', views.about, name='about'),
     path('rango/', include('rango.urls')),
+    path('static/', views.static, name='static'),
+    path('rango/', include('rango.urls')),
+    path('static/images/rango.jpeg', views.rangojpg, name='static/image/rango.jpg'),
+    path('rango/', include('rango.urls')),
     # The above maps any URLs starting with rango/ to be handled by rango.
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 
